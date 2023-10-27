c736f63f56e0b6e4367588e7221429ca5c9c8a1b
Jesitha15 <148961758+Jesitha15@users.noreply.github.com>
2023-10-27 15:53:38 +0530

    Update stg_annual_enterprise_survey_02.sql

--- models/example/stg_annual_enterprise_survey_02.sql
+++ models/example/stg_annual_enterprise_survey_02.sql
@@ -4,10 +4,10 @@ With
 	one AS 
 	(
 	select 
-	id AS table1_id,year AS table1_year,industry_code_ANZSIC AS table1_industry_code,
-  	industry_name_ANZSIC AS table1_industry_name,rme_size_grp AS table1_rme_size_grp,
-  	variable AS table1_variable,value AS table1_value,unit AS table1_unit,
-  	serial_number AS table1_serial_number,serial_number_1 AS table1_serial_number_1 
+	id ,year,industry_code_ANZSIC,
+  	industry_name_ANZSIC ,rme_size_grp ,
+  	variable,value ,unit,
+  	serial_number ,serial_number_1 
   	from stg02
 	)
 	select * from one
