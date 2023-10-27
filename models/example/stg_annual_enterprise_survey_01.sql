df6146f977aeb1a583c96d93aa969c5e9426858b
Jesitha15 <148961758+Jesitha15@users.noreply.github.com>
2023-10-27 15:37:37 +0530

    Add files via upload

---
+++ models/example/stg_annual_enterprise_survey_01.sql
@@ -0,0 +1,13 @@
+With
+	stg01 AS (
+	select * from `cloudside-academy.dbt_sample_data.annual-enterprise-survey01`)
+	one AS
+	(
+	select 
+	id AS table2_id,Year AS table2_year,Industry_aggregation_NZSIOC AS table2_industry_aggregation,
+	Industry_code_NZSIOC AS table2_industry_code,Industry_name_NZSIOC AS table2_industry_name,
+	Units AS table2_units,Value AS table2_value,Industry_code_ANZSIC06 AS table2_industry_code_ANZSIC06,
+	serial_number AS table2_serial_number,serial_number_1 AS table2_serial_number_1
+	from stg01
+	)
+	select * form one
