fde99d8665a707a06e8ce528edaea3042484b133
Jesitha15 <148961758+Jesitha15@users.noreply.github.com>
2023-10-27 16:29:18 +0530

    Update stg_annual_enterprise_survey_02.sql

--- models/example/stg_annual_enterprise_survey_02.sql
+++ models/example/stg_annual_enterprise_survey_02.sql
@@ -1,14 +1,14 @@
 With 
 	stg02 AS (
-	select * from `cloudside-academy.dbt_sample_data.annual-enterprise-survey02`),
-	one AS 
-	(
+	select * from `cloudside-academy.dbt_sample_data.annual-enterprise-survey02`)
+
+
 	select 
 	id ,year,industry_code_ANZSIC,
   	industry_name_ANZSIC ,rme_size_grp ,
   	variable,value ,unit,
   	serial_number ,serial_number_1 
   	from stg02
-	)
-	select * from one
+	
+	
 	
