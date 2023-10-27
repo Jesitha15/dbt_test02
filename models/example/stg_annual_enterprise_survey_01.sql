aac78481f3598fe9d30065c87cf8ccf4ffc18d8d
Jesitha15 <148961758+Jesitha15@users.noreply.github.com>
2023-10-27 16:29:51 +0530

    Update stg_annual_enterprise_survey_01.sql

--- models/example/stg_annual_enterprise_survey_01.sql
+++ models/example/stg_annual_enterprise_survey_01.sql
@@ -1,13 +1,11 @@
 With
 	stg01 AS (
-	select * from `cloudside-academy.dbt_sample_data.annual-enterprise-survey01`),
-	one AS
-	(
+	select * from `cloudside-academy.dbt_sample_data.annual-enterprise-survey01`)
+	
 	select 
 	id ,Year,Industry_aggregation_NZSIOC ,
 	Industry_code_NZSIOC,Industry_name_NZSIOC ,
 	Units ,Value ,Industry_code_ANZSIC06 ,
 	serial_number ,serial_number_1 
 	from stg01
-	)
-	select * from one
+	
