77cc0e1089077c1eea572369cc1675caa7e140b7
Jesitha15 <148961758+Jesitha15@users.noreply.github.com>
2023-10-27 15:42:12 +0530

    Update survey.sql

--- models/example/survey.sql
+++ models/example/survey.sql
@@ -1,6 +1,6 @@
 WITH
 	{{config(materialized='table')}}
 	survey as (
-	SELECT * FROM {{ref(`inter01_annual_enterprise_survey`)}} 
+	SELECT * FROM {{ref('inter01_annual_enterprise_survey')}} 
 	)
 	SELECT * FROM survey
